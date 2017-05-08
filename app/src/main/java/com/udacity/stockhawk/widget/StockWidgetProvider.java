edda42a99a44266fe50a876bff891bec3e858c75
DuoPegla <josip.sarlija@gmail.com>
2017-05-08 23:39:45 +0200

    Opens detail activity from widget

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -3,13 +3,16 @@ package com.udacity.stockhawk.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.sync.QuoteSyncJob;
 import com.udacity.stockhawk.ui.MainActivity;
+import com.udacity.stockhawk.ui.StockDetailActivity;
 
 /**
  * Created by duopegla on 8.5.2017..
@@ -17,23 +20,61 @@ import com.udacity.stockhawk.ui.MainActivity;
 
 public class StockWidgetProvider extends AppWidgetProvider
 {
+//    @Override
+//    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+//        for (int appWidgetId : appWidgetIds)
+//        {
+//            Intent intent = new Intent(context, MainActivity.class);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+//
+//            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_initial);
+//            remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+//
+//            Intent widgetIntent = new Intent(context, StockWidgetService.class);
+//            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+//            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+//
+//            remoteViews.setRemoteAdapter(R.id.widget_list, widgetIntent);
+//
+//            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+//        }
+//    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds)
-        {
+        for(int appWidgetId : appWidgetIds){
             Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_initial);
-            remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.widget_initial);
 
             Intent widgetIntent = new Intent(context, StockWidgetService.class);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-
+            context.startService(widgetIntent);
             remoteViews.setRemoteAdapter(R.id.widget_list, widgetIntent);
 
-            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+            // Intent to launch Main Activity
+            Intent mainIntent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Intent to launch Detail Stock Activity
+            Intent detailIntent = new Intent(context, StockDetailActivity.class);
+            PendingIntent pendingIntentDetail = PendingIntent.getActivity(context, 0, detailIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setPendingIntentTemplate(R.id.widget_list, pendingIntentDetail);
+
+            appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list);
+
+
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(intent.getAction().equals(QuoteSyncJob.ACTION_DATA_UPDATED)) {
+            AppWidgetManager am = AppWidgetManager.getInstance(context);
+            int [] appWidgetIds = am.getAppWidgetIds(new ComponentName(context, StockWidgetProvider.class));
+            am.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        }
+        super.onReceive(context, intent);
     }
 }
